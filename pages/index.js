d8ba2b55ad341f86c270b5e234f067e3bbf84a7e
bakir Diallo <bakirdiallo@MacBook-Air-de-bakir.local>
2025-04-08 01:01:34 +0200

    🔓 Accès direct à /home - désactivation du pré-lancement

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,9 @@ import { useRouter } from 'next/router'
 export default function IndexRedirect() {
   const router = useRouter()
 
-  useEffect(() => {
-    const launchDate = new Date('2025-04-20T12:00:00') // Change si besoin
-    const now = new Date()
-    if (now < launchDate) {
-      router.replace('/prelaunch') // Redirige vers landing avant la date
-    } else {
-      router.replace('/home') // Redirige vers l'accueil après lancement
-    }
-  }, [])
+useEffect(() => {
+  router.replace('/home')
+}, [])
 
   return null
 }
